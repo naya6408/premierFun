d2e4ddf93a32ef5feb59beb5a0f44f88803d09bb
Junsup Lee <jlee651@illinois.edu>
2019-11-02 14:24:38 -0500

    adding data frame of teams

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ import seaborn as sb
 import json
 import pandas as pd 
 
+# def compareStats(teamOne, teamTwo):
+
 url = "https://sportdata.p.rapidapi.com/api/v1/free/soccer/standings/premier-league"
 
 headers = {
-
+	'x-rapidapi-host': "sportdata.p.rapidapi.com",
+	'x-rapidapi-key': "8ed5762f77msh184b3093218aeddp1cd8a4jsn6c675ec1a175"
 }
 
 response = re.get(url, headers=headers)
 response = response.json()
 
-print(json.dumps(response, sort_keys=True, indent=4))
-print(type(response))
-print(len(response))
-
 data = []
+teamList = []
+outputdict = {}
 
 for i in range(len(response)):
-    for key,value in response[i].items():
-        if(key != "teamname"):
-            data.append([key,value])
+	for key,value in response[i].items():
+		if(key == "teamname"):
+			teamList.append(value)
 
+print(json.dumps(response, sort_keys=True, indent=4))
+
+
+df = pd.DataFrame(teamList)
 print("data is:", data)
+print("Data Frame containing list of teams is", df)
+
+
+
+
 
 
 
